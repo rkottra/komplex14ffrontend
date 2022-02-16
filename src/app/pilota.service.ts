13a6fc9aa1ce76af909bc4004491b7b644e32e5a
Kottra Richárd <kottra.richard@jedlik.eu>
2022-02-16 14:23:15 +0100

    delete

--- src/app/pilota.service.ts
+++ src/app/pilota.service.ts
@@ -9,13 +9,25 @@ import { PilotaModel } from './models/pilota.model';
 })
 export class PilotaService {
   
-  constructor(private http:HttpClient) {
+  public token:string = "";
 
+  constructor(private http:HttpClient) {
+      this.http.get("http://localhost:8000/api/token", {responseType: 'text'}).toPromise().then(
+        (szerverrolvissza) => {
+          this.token = szerverrolvissza;
+          console.log(this.token);
+        }
+      )
   }
 
   getPilotak():Observable<PilotaModel[]> {
     return this.http
-            .get<any[]>("http://localhost:8000/api/pilotak").pipe(
+            .get<any[]>("http://localhost:8000/api/pilotak", {headers: {
+              'X-CSRF-TOKEN': this.token,
+              'token': this.token,
+              '_token': this.token,
+              'csrf-token': this.token,
+            }}).pipe(
               map((adatok) => {
                 let seged: PilotaModel[] = [];
                 
@@ -33,7 +45,13 @@ export class PilotaService {
   }
 
   deletePilota(pilotaId: number):Observable<any> {
-    return this.http.delete("http://localhost:8000/api/pilotak", {'params': {pilotaid: pilotaId}})
+    return this.http.delete("http://localhost:8000/api/pilotak/"+pilotaId, {headers: {
+        'X-CSRF-TOKEN': this.token,
+        'token': this.token,
+        '_token': this.token,
+        'csrf-token': this.token,
+      }
+    })
   }
 
 }
