26f6b2f0629c852d073699c3dab6c6654706c6a9
Kottra Richárd <kottra.richard@jedlik.eu>
2022-03-02 14:06:58 +0100

    dialogus ablak

--- src/app/pilota/pilota.component.ts
+++ src/app/pilota/pilota.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { PilotaModel } from '../models/pilota.model';
 import { PilotaService } from '../pilota.service';
 
+interface Nemzetiseg {
+  name: string,
+  code: string
+}
+
 
 @Component({
   selector: 'app-pilota',
@@ -12,19 +17,43 @@ export class PilotaComponent implements OnInit {
 
   public pilotak: PilotaModel[] = []; 
 
+  public ujpilota: PilotaModel = new PilotaModel();
+  display: boolean = false;
+  
+  public nemzetisegek: Nemzetiseg[];
+
+
   constructor(private szerviz:PilotaService) { 
+
+      this.nemzetisegek = [
+          {name: 'angol', code: 'angol'},
+          {name: 'finn', code: 'finn'},
+          {name: 'német', code: 'német'},
+      ];
 
       this.szerviz.getPilotak().subscribe( adatok => {
        
         this.pilotak = adatok;
 
-        this.pilotak[7].csapat.nemzet = "asd";
+        //this.pilotak[7].csapat.nemzet = "asd";
       })
   }
 
   ngOnInit(): void {
   }
 
+  showDialog() {
+    this.display = !this.display;
+  }
+
+  save() {
+    alert(this.ujpilota);
+    this.display = false;
+  }
+
+  cancel() {
+    this.display = false;
+  }
   
   torles(pilotaId:number) {
     this.szerviz.deletePilota(pilotaId).subscribe();
